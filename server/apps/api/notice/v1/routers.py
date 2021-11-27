a1e0c8028b93362a68e544f7f768cc46cc111641
Nikita <grishchenkonikita@mail.ru>
2021-11-28 01:25:57 +0300

    get for group notice

--- server/apps/api/notice/v1/routers.py
+++ server/apps/api/notice/v1/routers.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views import SendNoticeListAPIView, SendNoticeRetrieveAPIView, GroupsEventsView
+from .views import (
+    AllGroupNoticeList,
+    GroupsEventsView,
+    NoticeOfOneGroupNoticeList,
+    SendNoticeListAPIView,
+    SendNoticeRetrieveAPIView,
+)
 
 app_name = "notice"
 
@@ -8,4 +14,6 @@ urlpatterns = [
     path("groups-events/", GroupsEventsView.as_view()),
     path("list/", SendNoticeListAPIView.as_view()),
     path("detail/<int:pk>/", SendNoticeRetrieveAPIView.as_view()),
+    path("all-group/", AllGroupNoticeList.as_view()),
+    path("for-group/<int:group>/", NoticeOfOneGroupNoticeList.as_view()),
 ]
--- server/apps/api/notice/v1/views.py
+++ server/apps/api/notice/v1/views.py
@@ -1,8 +1,11 @@
+from enum import unique
+
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from ..models import SendNotice, GroupEvent
-from .serializers import SendNoticeSerializer, GroupEventSerializer
+from ..models import GroupEvent, SendNotice
+from .serializers import GroupEventSerializer, SendNoticeSerializer
 
 
 class GroupsEventsView(generics.ListAPIView):
@@ -15,7 +18,6 @@ class GroupsEventsView(generics.ListAPIView):
     queryset = GroupEvent.objects.all()
 
 
-
 class SendNoticeListAPIView(generics.ListAPIView):
     """
     Список уведомлений
@@ -26,7 +28,9 @@ class SendNoticeListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        queryset = SendNotice.objects.filter(user_id=user.pk).order_by('notice__event__priority', 'date_of_send')
+        queryset = SendNotice.objects.filter(user_id=user.pk).order_by(
+            "notice__event__priority", "date_of_send"
+        )
         return queryset
 
 
@@ -42,3 +46,70 @@ class SendNoticeRetrieveAPIView(generics.RetrieveAPIView):
         user = self.request.user
         queryset = SendNotice.objects.filter(user_id=user.pk)
         return queryset
+
+
+class AllGroupNoticeList(generics.ListAPIView):
+    """Отображение списка всех групп уведомлений  имеющихся у пользователя """
+
+    serializer_class = SendNoticeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = SendNotice.objects.filter(user_id=user.pk)
+
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        response_list = serializer.data
+
+        # получение списка групп
+        group_notice = []
+        for item in response_list:
+            group_notice.append(dict(item["notice"]["event"]["group"]))
+
+        # удаление повторов в списке объектов
+        seen_names = set()
+        unique_group_notice = []
+        for item in group_notice:
+            if item["name"] not in seen_names:
+                unique_group_notice.append(item)
+                seen_names.add(item["name"])
+
+        # добавление поля count
+        for item in unique_group_notice:
+            item.update({"count": 0})
+
+        # подсчет количества уведомлений в каждой группе
+        for item_unique in unique_group_notice:
+            for item in group_notice:
+                if item_unique["id"] == item["id"]:
+                    item_unique["count"] += 1
+
+        return Response(unique_group_notice)
+
+
+class NoticeOfOneGroupNoticeList(generics.ListAPIView):
+    """ Список уведомления пользователя для выбранной группы уведомлений """
+
+    lookup_url_kwarg = "group"
+    serializer_class = SendNoticeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = SendNotice.objects.filter(user_id=user.pk)
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        response_list = serializer.data
+        group = self.kwargs["group"]
+        list_result = []
+        for item in response_list:
+            if item["notice"]["event"]["group"]["id"] == group:
+                list_result.append(item)
+        return Response(list_result)
