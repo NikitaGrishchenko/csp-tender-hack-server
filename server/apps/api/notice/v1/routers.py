3a5f054f2a579a7f9e410fced2c3f950ba1b815a
Nikita <grishchenkonikita@mail.ru>
2021-11-27 14:47:24 +0300

    create detail view

--- server/apps/api/notice/v1/routers.py
+++ server/apps/api/notice/v1/routers.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SendNoticeListAPIView
+from .views import SendNoticeListAPIView, SendNoticeRetrieveAPIView
 
 app_name = "notice"
 
 urlpatterns = [
     path("list/", SendNoticeListAPIView.as_view()),
+    path("detail/<int:pk>/", SendNoticeRetrieveAPIView.as_view()),
 ]
--- server/apps/api/notice/v1/views.py
+++ server/apps/api/notice/v1/views.py
@@ -6,6 +6,24 @@ from .serializers import SendNoticeSerializer
 
 
 class SendNoticeListAPIView(generics.ListAPIView):
+    """
+    Список уведомлений
+    """
+
+    serializer_class = SendNoticeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = SendNotice.objects.filter(user_id=user.pk)
+        return queryset
+
+
+class SendNoticeRetrieveAPIView(generics.RetrieveAPIView):
+    """
+    Детальный обзор уведомления
+    """
+
     serializer_class = SendNoticeSerializer
     permission_classes = [IsAuthenticated]
 
